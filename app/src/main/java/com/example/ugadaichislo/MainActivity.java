ad35567aba234080261eaf68d63c854766796f8c
kseniya <kseniya.starover@mail.ru>
2020-11-16 11:06:38 +0300

    UgadaiChislo

---
+++ app/src/main/java/com/example/ugadaichislo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ugadaichislo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinished;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView)findViewById(R.id.textView1);
+        etInput = (EditText)findViewById(R.id.editText1);
+        bControl = (Button)findViewById(R.id.button1);
+
+        guess = (int)(Math.random() * 100);
+        gameFinished = false;
+    }
+
+    public void OnClick(View v){
+        if (!gameFinished){
+            int inp = Integer.parseInt(etInput.getText().toString());
+            if (inp > guess)
+                tvInfo.setText(getResources().getString(R.string.ahead));
+            if (inp < guess)
+                tvInfo.setText("Недолет!");
+            if (inp == guess) {
+                tvInfo.setText(getResources().getString(R.string.hit));
+                bControl.setText(getResources().getString(R.string.play_more));
+                gameFinished = true;
+            }
+        }
+        else {
+            guess = (int)(Math.random() * 100);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished = false;
+        }
+    }
+}
